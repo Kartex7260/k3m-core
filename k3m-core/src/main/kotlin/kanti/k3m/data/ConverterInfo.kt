47ab29bfe90fa70c7cba8b99f9c73b199f284c53
Kirill Zhitko <kirillzhitko@gmail.com>
2024-03-12 18:54:51 +0300

    REFACTOR: доработка ConverterInfo

--- k3m-core/src/main/kotlin/kanti/k3m/data/ConverterInfo.kt
+++ k3m-core/src/main/kotlin/kanti/k3m/data/ConverterInfo.kt
@@ -1,7 +1,15 @@
 package kanti.k3m.data
 
-data class ConverterInfo(
-	val function: TypeInfo,
-	val dependency: TypeInfo?,
-	val dependencyParameterName: String?
-)
+sealed class ConverterInfo {
+
+	class GlobalFunc(
+		val funcName: String,
+		val packageName: String
+	) : ConverterInfo()
+
+	class ClassFunc(
+		val function: String,
+		val type: TypeInfo,
+		val dependencyParameterName: String?
+	) : ConverterInfo()
+}
